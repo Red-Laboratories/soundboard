470063d52e256dbaec65df243cd9b2ba41d8366e
Andrew <andrewngo91@gmail.com>
2022-10-01 16:50:53 -0700

    buttons completed need to test

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import Board from './Board'
+import React, {useState} from 'react';
+import Button from './Button'
+
+// create a function to pull info from DB to create initial btnKey & sound state
+const getBtnKeys = async() => {
+    const state = await fetch('/');
+    // edit state?
+    return state;
+}
+
+const getSound = async() => {
+    const state = await fetch('/');
+    return state;
+}
+// create a function to make patch request to update database when we update state
+
+btnKey = [
+    Q,W,E,
+
+]
 
 function App() {
+    
+    const [btnKey, changeKey] = useState(getBtnKeys()); //pull from data        
+    const [sound, changeSound] = useState(getSound());
 
+    console.log(btnKey);
+    console.log(sound);
+    const board = []
+    for(i = 0; i < 9; i++){
+        board.push(<Button key={i} btnKeyVal={btnKey[i]} soundVal={sound[i]}/>)
+    }
+        
     return (
         <main>
             <header>
@@ -10,7 +38,7 @@ function App() {
                     Hello World
                 </div>
             </header>
-            <Board/>
+            {board}
         </main>
     )
 
@@ -18,5 +46,4 @@ function App() {
 
 export default App;
 
-//tailwind not working
 
--- client/components/Button.js
+++ client/components/Button.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 
 function Button () {
-    
-    
-    return(
-        <div className="button"></div>
-    )
+    <div class="grid grid-cols-3 gap-3 p-5 mt-12">
+          <div class="w-72 h-72 text-black self-center hover:shadow-lg transition-all ease-in-out h-30 rounded-xl bg-gray-400 p-4 cursor-pointer">
+            <span>
+                Q
+                <button className="hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base align-text-top rounded-full">
+                    ...
+                </button>
+            </span>
+            
+          </div>
+          <div class=" w-72 h-72 shadow-lg text-black self-center hover:shadow-lg transition-all ease-in-out h-30 rounded-xl bg-gray-400 p-4 cursor-pointer">W</div>
+          <div class=" w-72 h-72 shadow-lg text-black self-center hover:shadow-lg transition-all ease-in-out h-30 rounded-xl bg-gray-400 p-4 cursor-pointer">E</div>
+        </div>
 }
 
 export default Button;
