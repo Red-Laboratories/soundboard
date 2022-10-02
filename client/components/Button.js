3a4f87020e99a19f2103bd014ab6397c13606923
Andrew <andrewngo91@gmail.com>
2022-10-01 17:46:21 -0700

    jj updates to button - not complete

--- client/components/Button.js
+++ client/components/Button.js
@@ -3,19 +3,18 @@ import React from 'react';
 
 function Button () {
     return(
-        <div class="grid grid-cols-3 gap-3 p-5 mt-12">
-        <div class="w-72 h-72 text-black self-center hover:shadow-lg transition-all ease-in-out h-30 rounded-xl bg-gray-400 p-4 cursor-pointer">
-          <span>
-              Q
-              <button className="hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base align-text-top rounded-full">
-                  ...
-              </button>
-          </span>
+      <div class="w-72 h-72 text-black self-center hover:shadow-lg transition-all ease-in-out h-30 rounded-xl bg-gray-400 p-4 cursor-pointer">
+        <div>
+          <p className="float-left">
+            Q
+          </p>
+          <button className="float-right align-middle hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base rounded-full">
+                ...
+          </button>
         </div>
       </div>
     )
 }
-   
    
 
 export default Button;
