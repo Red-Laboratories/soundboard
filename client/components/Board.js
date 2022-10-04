d4395b97e51925993a75e694e213c2e22ec17640
Mark Nichting <mtnichting@gmail.com>
2022-10-03 17:44:11 -0700

    login and logout functionality completed
    Co-authored-by: Tracy Chang
    Co-authored-by: Johnjered

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,6 +1,33 @@
 import React, { useState } from 'react';
 import Button from './Button';
 
+// make get request to server to verify if there is cookie first
+const checkCookie = () => {
+  // make get request to which endpoint?
+  fetch('/api/main')
+    .then(res => res.json())
+    .then(data => {
+      if (data === 'unauthorized') {
+        window.location.href = '/';
+      } else {
+        console.log('welcome back!')
+      }
+    })
+  .catch(err =>console.log('error with checkCookie in Board.js'))
+};
+
+// create a logout function
+const logout = () => {
+  fetch('/api/logout', {
+    method: 'DELETE'
+  })
+  .then(res => res.json())
+  .then(data => {
+    window.location.href = '/';
+  })
+  .catch(err =>console.log('error with logout in Board.js'))
+};
+
 // create a function to pull info from DB to create initial btnKey & sound state
 const getBtnKeys = async () => {
   const state = await fetch('/');
@@ -17,6 +44,7 @@ const getSound = async () => {
 
 
 function Board() {
+  checkCookie();
   // pass evaluated result of getSound and getBtnKEys to useState for our initial state
   const [btnKey, changeKey] = useState(["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]);
   const [sound, changeSound] = useState(["Q", "W", "E", "A", "S", "D", "Z", "X", "C"]);
@@ -29,6 +57,7 @@ function Board() {
   }
   return (
     <div>
+          <button onClick={logout}>Logout</button>
       <div className="bg-slate-400 py-2 px-2 border-b-8 border-slate-500 rounded-xl border">
         <h1 className="font-bold text-3xl text-center text-slate-300">SoundBoard</h1>
         <div className="grid grid-cols-3 gap-4">
--- client/components/Login.js
+++ client/components/Login.js
@@ -1,15 +1,73 @@
 import React from 'react';
 
 
+function loginClick() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  console.log(username, password);
+  if (!username) alert('Need to enter a username!');
+  if (!password) alert('Need to enter a password!');
+  // make post request to server with username and password
+  fetch('api/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      if (data === 'failed login') {
+        // send alert if login attemp failed
+        alert('you have entered an incorrect username/password')
+      } else {
+        // if verified redirect to homepage
+        window.location.href = '/board'
+      }
+    })
+    .catch(err => console.log('Login error: ', err));
+}
+
 function Login () {
     return(
       <div>
-        <input type='text' placeholder='username'/>
-        <input type='password' placeholder='password'/>
-        <button onClick={() => {window.location.href='/board'}}>login</button>
+        <input type='text' id ='username' placeholder='username'/>
+        <input type='password' id ='password' placeholder='password'/>
+        <button onClick={loginClick}>login</button>
       </div>
     )
 }
    
 
 export default Login;
+
+/*app.post('/', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/board');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});*/
